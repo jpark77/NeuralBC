8a6b15e2c4d89fc296f20251ef34314e40e2c582
unknown <yhnr1115@gmail.com>
2018-07-25 13:56:27 +0900

    proto three revised

--- contract/proto_three_final.sol
+++ contract/proto_three_final.sol
@@ -23,26 +23,26 @@ contract SafeMath {
 contract TokenExchange is SafeMath {
     
     address public token;
-    uint min_ratio;
-    uint max_ratio;
+    uint public min_ratio;
+    uint public max_ratio;
     
-    uint eth_max_amt;
-    uint tok_max_amt;
-    uint eth_min_amt;
-    uint tok_min_amt;
+    uint public eth_max_amt;
+    uint public tok_max_amt;
+    uint public eth_min_amt;
+    uint public tok_min_amt;
     
     mapping (address => uint) public eth_amt;
     mapping (address => uint) public tok_amt;
     mapping (uint => address) public eth_users;
     mapping (uint => address) public tok_users;
 
-    uint total_eth;
-    uint total_tok;
-    uint ratio_eth;
-    uint ratio_tok;
+    uint public total_eth;
+    uint public total_tok;
+    uint public ratio_eth;
+    uint public ratio_tok;
     
-    uint cnt_eth;
-    uint cnt_tok;
+    uint public cnt_eth;
+    uint public cnt_tok;
     uint amt_get;
 
     uint unit;
@@ -50,55 +50,58 @@ contract TokenExchange is SafeMath {
     uint ratio;
     uint for_ratio;
 
-    bool TimetoDeposit;
-    bool TimetoWithdraw;
+    bool public TimetoDeposit;
 
-    function TokenExchange(address _token, uint _min_ratio, uint _max_ratio, uint _eth_max_amt, uint _tok_max_amt){
-        require(_token != 0);
-        
+    uint i;
+    uint j;
+    
+    uint256 public debug;
+
+    function TokenExchange(address _token, uint _min_ratio, uint _max_ratio, uint _eth_max_amt){
+        require(_max_ratio>=_min_ratio);
         token = _token;
+
         max_ratio = _max_ratio;
         min_ratio = _min_ratio;
-        eth_max_amt = _eth_max_amt;
-        tok_max_amt = _tok_max_amt;
+        ratio = 4;
+        for_ratio = 10**ratio;
         ratio_eth = 0;
         ratio_tok = 0;
+
+        eth_max_amt = _eth_max_amt;
+        tok_max_amt = eth_max_amt * max_ratio / for_ratio;
+        eth_min_amt = 1*(10**18);
+        tok_min_amt = (min_ratio+max_ratio)*eth_min_amt/(2*for_ratio);
         
         total_eth = 0;
         total_tok = 0;
         cnt_eth = 0;
         cnt_tok = 0;
         
-        eth_min_amt = 1;
-        tok_min_amt = (min_ratio+max_ratio)/2 * eth_min_amt;
-        unit = 10;
-        ratio = 4;
-        for_unit = 10**unit;
-        for_ratio = 10**ratio;
-        
         TimetoDeposit = true;
     }
     
-    function depositEther() payable {
+    function depositEther() payable public {
         require(TimetoDeposit);
         require(msg.value >= eth_min_amt); //check min allowance
-        require(total_eth+msg.value < eth_max_amt); //check max allowance
+        require(total_eth+msg.value <= eth_max_amt); //check max allowance
         require(total_eth+msg.value > total_eth); //prevent overflow
-
+        
         //update user, user's amount
         eth_amt[msg.sender] = safeAdd(eth_amt[msg.sender], msg.value);
         eth_users[cnt_eth++] = msg.sender;
         
         //update total amount, ratio
         total_eth += msg.value;
-        ratio_eth = total_tok*for_unit / total_eth;
-        ratio_tok = (total_tok==0)? 0 : total_eth*for_unit / total_tok;
+        
+        ratio_eth = total_tok*for_ratio / total_eth;
+        ratio_tok = (total_tok==0)? 0 : total_eth*for_ratio / total_tok;
     }
 
-    function depositToken(uint amount) payable {
+    function depositToken(uint amount) {
         require(TimetoDeposit);
-        require(amount > tok_min_amt); //check min allowance
-        require(total_tok+amount < tok_max_amt); //check max allowance
+        require(amount >= tok_min_amt); //check min allowance
+        require(total_tok+amount <= tok_max_amt); //check max allowance
         require(total_tok+amount > total_tok); //prevent overflow
         
         //deposit
@@ -109,9 +112,10 @@ contract TokenExchange is SafeMath {
         
         //update total amount, ratio
         total_tok += amount;  
-        ratio_eth = (total_eth==0)? 0 : total_tok*for_unit / total_eth;
-        ratio_tok = total_eth*for_unit / total_tok;        
+        ratio_eth = (total_eth==0)? 0 : total_tok*for_ratio / total_eth;
+        ratio_tok = total_eth*for_ratio / total_tok;        
     }
+
 
     function exchange(){
         TimetoDeposit = false;
@@ -119,10 +123,10 @@ contract TokenExchange is SafeMath {
         uint amt_temp;
 
         //ether -> token
-        uint i=0;
-        while(i<=cnt_eth){
+        i=0;
+        while(i<cnt_eth){
             if(total_tok > 0){ //exchange
-                if(total_tok-eth_amt[eth_users[i]]*ratio_eth < 0){ //send token partly
+                if(total_tok-(eth_amt[eth_users[i]]*ratio_eth/for_ratio) < 0){ //send token partly
                     amt_temp = total_tok*for_ratio/ratio_eth;
                     eth_amt[eth_users[i]] -= (amt_temp+1);
                     if (!Token(token).transfer(tok_users[i], total_tok)) throw;
@@ -132,31 +136,35 @@ contract TokenExchange is SafeMath {
                     if (!Token(token).transfer(tok_users[i], amt_send*ratio_eth/for_ratio)) throw;
                     break;
                 }
+            } else {
+                break;
             }
         }
-        while(i<=cnt_eth){ //send back ether
+        while(i<cnt_eth){ //send back ether
             if (!eth_users[i].call.value(eth_amt[eth_users[i]])()) throw;
             i++;
         }
         
         //token -> ether
-        uint j=0;
-        while(j<=cnt_tok){
+        j=0;
+        while(j<cnt_tok){
             if(total_eth > 0){ //exchange
-                if(total_eth-tok_amt[tok_users[j]]*ratio_tok < 0){ //send ether partly
+                if(total_eth-(tok_amt[tok_users[j]]*ratio_tok/for_ratio) < 0){ //send ether partly
                     amt_temp = total_eth*for_ratio/ratio_tok;
                     tok_amt[tok_users[j]] -= (amt_temp+1);
                     if (!tok_users[j].call.value(tok_amt[tok_users[j]])() ) throw;
                 } else { //send ether
                     amt_send = tok_amt[tok_users[j]];
                     tok_amt[tok_users[j]] = safeSub(tok_amt[tok_users[j]], amt_send);
-                    if (!tok_users[j].call.value(amt_send*ratio_tok/for_ratio)()) throw;
+                    if (!tok_users[j].call.value(amt_send*ratio_tok/for_unit)()) throw;
                     break;
                 }
+            } else {
+                break;
             }
             j++;
         }
-        while(j<=cnt_tok){ //send back token
+        while(j<cnt_tok){ //send back token
             amt_send = tok_amt[tok_users[j]];
             tok_amt[tok_users[j]] = safeSub(tok_amt[tok_users[j]], amt_send);
             if (!Token(token).transfer(tok_users[j], amt_send)) throw;
@@ -164,7 +172,7 @@ contract TokenExchange is SafeMath {
         }    
 
     }
-    
+   
 }
 
 contract Token {
@@ -181,7 +189,5 @@ contract Token {
   uint public decimals;
   string public name;
 }
-
-
 
 
