d454c1ff2766f9827f8e14c3b6e47ec7c49d883d
unknown <yhnr1115@gmail.com>
2018-07-25 18:42:49 +0900

    proto three revised

--- contract/proto_three_final.sol
+++ contract/proto_three_final.sol
@@ -38,15 +38,17 @@ contract TokenExchange is SafeMath {
 
     uint public total_eth;
     uint public total_tok;
+    uint public rest_eth;
+    uint public rest_tok;
     uint public ratio_eth;
     uint public ratio_tok;
     
     uint public cnt_eth;
     uint public cnt_tok;
     uint amt_get;
+    uint amt_send;
+    uint amt_temp;
 
-    uint unit;
-    uint for_unit;
     uint ratio;
     uint for_ratio;
 
@@ -55,8 +57,6 @@ contract TokenExchange is SafeMath {
     uint i;
     uint j;
     
-    uint256 public debug;
-
     function TokenExchange(address _token, uint _min_ratio, uint _max_ratio, uint _eth_max_amt){
         require(_max_ratio>=_min_ratio);
         token = _token;
@@ -71,7 +71,8 @@ contract TokenExchange is SafeMath {
         eth_max_amt = _eth_max_amt;
         tok_max_amt = eth_max_amt * max_ratio / for_ratio;
         eth_min_amt = 1*(10**18);
-        tok_min_amt = (min_ratio+max_ratio)*eth_min_amt/(2*for_ratio);
+        tok_min_amt = eth_min_amt*min_ratio/for_ratio;
+        //tok_min_amt = (min_ratio+max_ratio)*eth_min_amt/(2*for_ratio);
         
         total_eth = 0;
         total_tok = 0;
@@ -107,7 +108,7 @@ contract TokenExchange is SafeMath {
         //deposit
         if (!Token(token).transferFrom(msg.sender, address(this), amount)) throw;
         //update user, users's amount
-        tok_amt[msg.sender] = safeAdd(tok_amt[msg.sender], msg.value);
+        tok_amt[msg.sender] = safeAdd(tok_amt[msg.sender], amount);
         tok_users[cnt_tok++]=msg.sender;
         
         //update total amount, ratio
@@ -119,22 +120,35 @@ contract TokenExchange is SafeMath {
 
     function exchange(){
         TimetoDeposit = false;
-        uint amt_send;
-        uint amt_temp;
+
+        rest_eth = total_eth;
+        rest_tok = total_tok;
+
+        if(ratio_eth<min_ratio){
+            ratio_eth = min_ratio;
+            ratio_tok = 10**8/ratio_eth;
+        } else if(ratio_eth>max_ratio){
+            ratio_eth = max_ratio;
+            ratio_tok = 10*8/ratio_eth;
+        }
 
         //ether -> token
         i=0;
         while(i<cnt_eth){
-            if(total_tok > 0){ //exchange
-                if(total_tok-(eth_amt[eth_users[i]]*ratio_eth/for_ratio) < 0){ //send token partly
-                    amt_temp = total_tok*for_ratio/ratio_eth;
-                    eth_amt[eth_users[i]] -= (amt_temp+1);
-                    if (!Token(token).transfer(tok_users[i], total_tok)) throw;
-                } else { //send token
-                    amt_send = eth_amt[tok_users[j]];
-                    eth_amt[eth_users[i]] = safeSub(eth_amt[eth_users[i]], amt_send);
-                    if (!Token(token).transfer(tok_users[i], amt_send*ratio_eth/for_ratio)) throw;
+            if(rest_tok > 0){ //exchange
+                if( (eth_amt[eth_users[i]]*ratio_eth/for_ratio) > rest_tok ){ //send token partly
+                    amt_temp = rest_tok*for_ratio/ratio_eth;
+                    eth_amt[eth_users[i]] -= amt_temp;
+                    if (!Token(token).transfer(tok_users[i], rest_tok)) throw;
+                    rest_tok = 0;
                     break;
+                } else { //send token
+                    amt_send = eth_amt[eth_users[i]];
+                    eth_amt[eth_users[i]] = safeSub(eth_amt[eth_users[i]], amt_send);
+                    rest_tok = safeSub(rest_tok, amt_send*ratio_eth/for_ratio);
+                    amt_send = (amt_send*ratio_eth)/for_ratio;
+                    if (!Token(token).transfer(tok_users[i], amt_send)) throw;
+                    i++;
                 }
             } else {
                 break;
@@ -148,16 +162,20 @@ contract TokenExchange is SafeMath {
         //token -> ether
         j=0;
         while(j<cnt_tok){
-            if(total_eth > 0){ //exchange
-                if(total_eth-(tok_amt[tok_users[j]]*ratio_tok/for_ratio) < 0){ //send ether partly
-                    amt_temp = total_eth*for_ratio/ratio_tok;
-                    tok_amt[tok_users[j]] -= (amt_temp+1);
-                    if (!tok_users[j].call.value(tok_amt[tok_users[j]])() ) throw;
+            if(rest_eth > 0){ //exchange
+                if( (tok_amt[tok_users[j]]*ratio_tok/for_ratio) > rest_eth ){ //send ether partly
+                    amt_temp = rest_eth*for_ratio/ratio_tok;
+                    tok_amt[tok_users[j]] -= amt_temp;
+                    if (!tok_users[j].call.value(rest_eth)() ) throw;
+                    rest_eth = 0;
+                    break;
                 } else { //send ether
                     amt_send = tok_amt[tok_users[j]];
                     tok_amt[tok_users[j]] = safeSub(tok_amt[tok_users[j]], amt_send);
-                    if (!tok_users[j].call.value(amt_send*ratio_tok/for_unit)()) throw;
-                    break;
+                    rest_eth = safeSub(rest_eth, amt_send*ratio_tok/for_ratio);
+                    amt_send = (amt_send*ratio_tok)/for_ratio;
+                    if (!tok_users[j].call.value(amt_send)()) throw;
+                    j++;
                 }
             } else {
                 break;
