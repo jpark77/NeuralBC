1c1ca3a6a7d1e2df06895973bd65114f643e45ec
yhnr15 <yhnr1115@gmail.com>
2018-08-21 16:55:21 +0900

    Create Rank1.1.sol

---
+++ vote_event/proto/Rank1.1.sol
@@ -0,0 +1,85 @@
+pragma solidity ^0.4.24;
+
+library SafeMath {
+
+  function mul(uint256 _a, uint256 _b) internal pure returns (uint256) {
+      if (_a == 0) {
+          return 0;
+        }
+      uint256 c = _a * _b;
+      assert(c / _a == _b);
+      return c;
+      
+  }
+
+  function div(uint256 _a, uint256 _b) internal pure returns (uint256) {
+      require(_b > 0);
+      uint256 c = _a / _b;
+      return c;
+  }
+
+  function sub(uint256 _a, uint256 _b) internal pure returns (uint256) {
+      assert(_b <= _a);
+      uint256 c = _a - _b;
+      return c;
+  }
+
+  function add(uint256 _a, uint256 _b) internal pure returns (uint256) {
+      uint256 c = _a + _b;
+      assert(c >= _a);
+      return c;
+  }
+}
+
+contract Rank{
+    using SafeMath for uint256;    
+    mapping(uint256 => address) ranking; // ranking_idx => ranker
+    mapping(address => uint256) ranker; // ranker => ranking_idx
+    mapping(uint256 => uint256) accum_count; //amount of upper rankers
+    mapping(uint256 => uint256) count;
+    
+    mapping(address => uint256) score; // score of User
+
+    
+    //mapping(address => bool) isNew;
+    
+    constructor(){
+        
+    }
+
+    
+    function scorePlus(address _user) returns (bool) {
+        uint256 nextScore;
+        uint256 originScore;
+        
+        originScore=score[_user];
+        count[originScore]=count[originScore].sub(1);
+        
+        nextScore=originScore.add(1);
+        score[_user]=nextScore; // plus one to score
+        accum_count[nextScore]=accum_count[nextScore].add(1);
+        
+        //change ranking_idx if there are people at the same rank group
+        if(count[originScore]!=0){
+            //exchange ranking
+            changeIndex(accum_count[originScore].add(1), ranker[_user]);
+        }
+        
+        count[nextScore]=count[nextScore].add(1);
+        return true;
+    }
+    
+    function changeIndex(uint256 idx_A, uint256 idx_B){
+        address temp = ranking[idx_A]; // ranker addr
+        ranking[idx_A] = ranking[idx_B];
+        ranking[idx_B] = temp;
+
+        ranker[ranking[idx_A]] = idx_A;
+        ranker[ranking[idx_B]] = idx_B;
+    }
+    
+}
+
+
+
+
